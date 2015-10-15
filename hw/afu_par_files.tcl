86e5d254fbea54742c3a85941fa7937ef38b95c9
peterpengwei <peng.wei.prc@cs.ucla.edu>
2015-10-15 14:57:02 -0700

    Add file list

---
+++ hw/afu_par_files.tcl
@@ -0,0 +1,3 @@
+set_global_assignment -name VERILOG_FILE /curr/pengwei/QuickAssist/Res_Reduce/hw/micro_bench.v
+set_global_assignment -name SYSTEMVERILOG_FILE /curr/pengwei/QuickAssist/Res_Reduce/hw/cci_std_afu.sv
+
